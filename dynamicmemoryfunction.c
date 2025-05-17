25f28e6790cdfd6226bf485099f8201b64755f14
jrishabharathi <rishabbharathi@gmail.com>
2025-05-17 06:38:06 +0530

    Create dynamicmemoryfunction.c

---
+++ dynamicmemoryfunction.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h> 
+int main() {
+    int *ptr;
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    ptr = (int *)malloc(n * sizeof(int));
+    if (ptr == NULL) {
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
+    printf("Enter %d integers:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &ptr[i]);
+    }
+    printf("You entered:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", ptr[i]);
+    }
+    printf("\n");
+    free(ptr);
+    return 0;
+}
