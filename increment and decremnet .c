1cfd1cc848c382dec29abf56c08beef8e427bee9
jrishabharathi <rishabbharathi@gmail.com>
2025-05-17 07:45:42 +0530

    Create increment and decremnet .c

---
+++ increment and decremnet .c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int x = 5;
+    printf("Initial value of x: %d\n", x);
+    x++;
+    printf("After increment (x++): %d\n", x);
+    x--;
+    printf("After decrement (x--): %d\n", x);
+    return 0;
+}
