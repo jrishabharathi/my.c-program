e00a44bc632b3da7ac391b7012f04e7159fa2cf7
jrishabharathi <rishabbharathi@gmail.com>
2025-05-17 07:38:35 +0530

    Create divisableby3&5.c

---
+++ divisableby3&5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    if (number % 3 == 0 && number % 5 == 0) {
+        printf("The number is divisible by both 3 and 5.\n");
+    } else {
+        printf("The number is NOT divisible by both 3 and 5.\n");
+    }
+
+    return 0;
+}
